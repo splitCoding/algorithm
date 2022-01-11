413fe3f0ff0e5cc4fe6b63f93f65710da19f2a37
splitube <voicesplit@daum.net>
2022-01-11 16:18:08 +0900

    Programmers_43163 DFS&BFS 단어변환 프로그래머스용 코드 주석으로 추가

--- DFS & BFS/Programmers_43164.java
+++ DFS & BFS/Programmers_43164.java
@@ -48,3 +48,57 @@ public class Programmers_43164 {
         }
     }
 }
+
+/*
+class Programmers_code {
+    ArrayList<String> finish, tmp;
+    String [][] tickets;
+    String [] root, final_root;
+    int root_idx = 1;
+    boolean is_used ;
+    boolean find;
+    public String[] solution(String[][] tickets) {
+        this.tickets = tickets;
+        root = new String[tickets.length+1];
+        final_root = new String[tickets.length+1];
+        is_used = new boolean[tickets.length];
+        tmp = new ArrayList<String>(tickets.length);
+        root[0] = "ICN";
+        dfs(0,root[0]);
+        return final_root;
+    }
+    void dfs(int depth, String start){
+        if(find)return;
+        if (depth == tickets.length) {
+            if(!find){
+                final_root = root.clone();
+                find = true;
+            }
+        }
+        if( find_next(start).length == 0){ return; }
+        String [] next = find_next(start);
+        for( int i=0;i<next.length;i++){
+            String [] next_info = next[i].split(",");
+            if(!is_used[Integer.parseInt(next_info[1])]){
+                is_used[Integer.parseInt(next_info[1])] = true;
+                root[root_idx++] = next_info[0];
+                dfs(depth+1, next_info[0]);
+                is_used[Integer.parseInt(next_info[1])] = false;
+                root_idx--;
+            }
+        }
+    }
+
+    String [] find_next(String start){
+        tmp.clear();
+        for(int i=0;i<tickets.length;i++){
+            if( tickets[i][0].equals(start)) {
+                tmp.add(tickets[i][1] + "," + i);
+            }
+        }
+        String [] arr = new String[tmp.size()];
+        Arrays.sort(tmp.toArray(arr));
+        return arr;
+    }
+}
+*/
