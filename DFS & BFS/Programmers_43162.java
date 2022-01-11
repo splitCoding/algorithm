e88f0a75af4fc698390d1566307816d5fcba4d98
splitube <voicesplit@daum.net>
2022-01-11 16:51:49 +0900

    Programmers_43162 DFS&BFS - 네트워크 프로그래머스 코드 추가

--- DFS & BFS/Programmers_43162.java
+++ DFS & BFS/Programmers_43162.java
@@ -12,10 +12,9 @@ public class Programmers_43162 {
         //i를 push하고 computers[pop()]과 연결된 인덱스를 push
         //이 과정을 스택이 빌때 까지 반복하면 i와 연결된 모든 컴퓨터들은 방문됨으로 전환
         for( int i=0;i< visited.length;i++){
-            if(!visited[i]){
-                dfs(i);
-                ret++;
-            }
+            if(visited[i])continue;
+            dfs(i);
+            ret++;
         }
         System.out.println(ret);
     }
@@ -34,3 +33,38 @@ public class Programmers_43162 {
         }
     }
 }
+/*
+import java.util.Stack;
+class Solution {
+    int n;
+    int [][] computers;
+    boolean [] visited;
+    Stack<Integer> s = new Stack<>();
+    int ret = 0;
+    public int solution(int n, int[][] computers) {
+        this.n = n;
+        this.computers = computers;
+        this.visited = new boolean[computers.length];
+        for( int i=0;i< visited.length;i++){
+            if(visited[i])continue;
+            dfs(i);
+            ret++;
+        }
+        return ret;
+    }
+    void dfs(int computer){
+        s.push(computer);
+        while(!s.empty()){
+            int next = s.pop();
+            visited[next] = true;
+            for( int i=0;i<computers[next].length;i++){
+                if( computers[next][i] == 1 && visited[i] == false){
+                    s.push(i);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+}
+ */
